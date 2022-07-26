3ef5bc77706a904f8f1b225c066fa012bce17365
Wando Borges <wanbrwan@gmail.com>
2022-07-26 14:39:09 -0300

    ReactJS app, consumindo api Github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import gitImg from './Assets/img/icon.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container text-center">
+      <h1 className="py-5 text-uppercase">Github profile</h1>
+      <form>
+        <div className="form-group">
+          <div className="input-group">
+            <input type="text" className="form-control" required />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-success">
+                Search
+              </button>
+            </span>
+          </div>
+        </div>
+      </form>
+      <div className="py-5">
+        <img src={gitImg} alt="github-image" height="200px" className="responsive rounded-circle" />
+
+        <h2 className="pt-5">
+          <a href="https://github.com/wtiinfo" target="_new" className="text-decoration-none">Wando Borges</a>
+        </h2>
+        <h3>
+          Ribeirão Pires - SP
+        </h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a href="#" target="_new" className="text-info text-decoration-none">www.wtiinfo.com</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
     </div>
   );
 }
